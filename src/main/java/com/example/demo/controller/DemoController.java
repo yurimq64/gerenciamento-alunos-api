8e7d5cf772015eabad55243accbe3c6982d12293
yuri <ymendesqueiroz@gmail.com>
2025-08-18 15:28:55 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hi, i'm here";
+    }
+
+    @GetMapping("/bye")
+    public String Bye() {
+        return "Bye, see you tomorrow";
+    }
+
+}
