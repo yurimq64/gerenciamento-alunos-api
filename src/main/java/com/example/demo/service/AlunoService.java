71ca9f735f61797bb99bfc58238b436f4c663248
yuri <ymendesqueiroz@gmail.com>
2025-08-26 22:06:28 -0300

    Criação de AlunoRepository e AlunoService

---
+++ src/main/java/com/example/demo/service/AlunoService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Aluno;
+import com.example.demo.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    private List<Aluno> findAllAlunos() {
+        return alunoRepository.findAll();
+    }
+
+    private Optional<Aluno> findAlunoById(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    private Aluno saveAluno(Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    private void deleteAluno(Long id) {
+        alunoRepository.deleteById(id);
+    }
+
+    private Aluno updateAluno(Long id,Aluno updatedAluno) {
+        return alunoRepository.findById(id)
+                .map(aluno -> {
+                    aluno.setNome(updatedAluno.getNome());
+                    aluno.setEmail(updatedAluno.getEmail());
+                    aluno.setDataNascimento(updatedAluno.getDataNascimento());
+                    aluno.setSenha(updatedAluno.getSenha());
+                    return alunoRepository.save(aluno);
+                }).orElseThrow(() -> new RuntimeException("Aluno não existe!"));
+    }
+}
