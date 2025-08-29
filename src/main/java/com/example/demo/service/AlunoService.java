f589e57ddbdff96a1de3ca65c12b399ae92e098d
yuri <ymendesqueiroz@gmail.com>
2025-08-28 21:51:28 -0300

    refactor: Métodos privados da classe AlunoService tornados públicos

--- src/main/java/com/example/demo/service/AlunoService.java
+++ src/main/java/com/example/demo/service/AlunoService.java
@@ -14,23 +14,23 @@ public class AlunoService {
     @Autowired
     private AlunoRepository alunoRepository;
 
-    private List<Aluno> findAllAlunos() {
+    public List<Aluno> findAllAlunos() {
         return alunoRepository.findAll();
     }
 
-    private Optional<Aluno> findAlunoById(Long id) {
+    public Optional<Aluno> findAlunoById(Long id) {
         return alunoRepository.findById(id);
     }
 
-    private Aluno saveAluno(Aluno aluno) {
+    public Aluno saveAluno(Aluno aluno) {
         return alunoRepository.save(aluno);
     }
 
-    private void deleteAluno(Long id) {
+    public void deleteAluno(Long id) {
         alunoRepository.deleteById(id);
     }
 
-    private Aluno updateAluno(Long id,Aluno updatedAluno) {
+    public Aluno updateAluno(Long id,Aluno updatedAluno) {
         return alunoRepository.findById(id)
                 .map(aluno -> {
                     aluno.setNome(updatedAluno.getNome());
