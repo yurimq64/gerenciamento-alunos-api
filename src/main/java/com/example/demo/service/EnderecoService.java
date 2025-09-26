8fb2a962e0dc8e125875e00e9e5e9670794cac09
yuri <ymendesqueiroz@gmail.com>
2025-09-25 21:26:44 -0300

    fix(EnderecoService): adiciona anotação @Service

--- src/main/java/com/example/demo/service/EnderecoService.java
+++ src/main/java/com/example/demo/service/EnderecoService.java
@@ -5,10 +5,12 @@ import com.example.demo.entity.Endereco;
 import com.example.demo.repository.AlunoRepository;
 import com.example.demo.repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EnderecoService {
 
     @Autowired
